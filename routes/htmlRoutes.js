9cc49fe8758727bdbd253d2a3d754de1f1381f79
Holly Phelps <Hollyphelps1992@gmail.com>
2020-03-22 20:29:44 -0700

    html routes updated

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+module.exports = function (app) {
+    
+   // each route handles the HTML page that the use gets sent to
+   app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+
+    
+ //stats page
+  app.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
+
+    //exercise page
+  app.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+  });
+}
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const db = require("./models");
 const app = express();
 
-app.use(logger("dev"));
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,3 +15,11 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlRoutes");
+
+
+app.listen(PORT, function () {
+    console.log(`Listing on Port: ${PORT}`);
+});
